45e7128b6707edd61a7a6572db976e28f6b0c37e
Luigi Morel <morelmiles@gmail.com>
2021-11-04 18:00:50 +0300

    Main component for the entire app

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { Features } from "../components/Features";
+import Footer from "../components/Footer";
+import GlobalMap from "../components/Global";
+import Hero from "../components/Hero";
 
 //Components
 import Navbar from "../components/Navbar";
+import Plans from "../components/Plans";
+import Testimonials from "../components/Testimonials";
 
 const Main = () => {
   return (
-    <div>
+    <div className=" ">
       <Navbar />
+      <Hero />
+      <Features />
+      <Plans />
+      <GlobalMap />
+      <Testimonials />
+      <Footer />
     </div>
   );
 };
