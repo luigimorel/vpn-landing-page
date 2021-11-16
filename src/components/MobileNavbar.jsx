b6af39708c7d2e1c2243c380a0bec0df86885a2e
Luigi Morel <morelmiles@gmail.com>
2021-11-16 08:30:29 +0300

    Add unique keys to the navbar list

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -35,7 +35,7 @@ const MobileNavbar = () => {
       <div className="sm:hidden dropdown ">
         <ul className=" bg-white mt-2.5 absolute min-w-full dropdown-menu ">
           {menuList.map((x) => (
-            <li className="active">
+            <li key={x.id} className="active">
               <NavLink
                 to={x.route}
                 className="block text-sm px-2 py-3 hover:text-primary hover:font-semibold"
